02ca21a395974cf0b88414bcacf5d48bb99c04d5
Lőrinc Nyitrai <lorinc@gmail.com>
2022-03-31 12:49:53 +0200

    Update util__every_journal_question.sql

--- util__every_journal_question.sql
+++ util__every_journal_question.sql
@@ -1,26 +1,38 @@
-select distinct
-	pj."version" as jversion,
-	pjsg."order" as gorder,
-	pjsg.title as gtitle,
-	pjs."order" as sorder,
-	pjs.title as stitle,
-	pjq."order" as qorder,
-	pjq."text" as qtext,
-	pjq."textFieldLabel" as qtextlabel,
-	pjqo."order" as oorder,
-	pjqo.value as ovalue
-from pwa_journey pj 
+create or replace view util_every_journal_questions as
 
-left join pwa_journey_step_group pjsg 
-on pj.id = pjsg.pwa_journey_id 
+	with results as (
+		select distinct
+			pj."version" as jversion,
+			pjsg."order"::text || '/'
+				|| pjs."order"::text || '/'
+				|| pjq."order"::text || '/'
+				|| pjqo."order"::text
+			as combined_order,
+			pjsg.title as gtitle,
+			pjs.title as stitle,
+			pjq."text" as qtext,
+			pjq."textFieldLabel" as qtextlabel,
+			pjqo.value as ovalue
+			
+		from pwa_journey pj 
+		
+		left join pwa_journey_step_group pjsg 
+		on pj.id = pjsg.pwa_journey_id 
+		
+		left join pwa_journey_step pjs 
+		on pjsg.id = pjs.pwa_journey_step_group_id
+		
+		left join pwa_journey_question pjq 
+		on pjs.id = pjq.pwa_journey_step_id 
+		
+		left join pwa_journey_question_option pjqo 
+		on pjq.id = pjqo.pwa_journey_question_id
+		
+		where pj.version = '1.0.0-naplo'
+		
+		order by combined_order
+	)
+	
+	select * from results
 
-left join pwa_journey_step pjs 
-on pjsg.id = pjs.pwa_journey_step_group_id
-
-left join pwa_journey_question pjq 
-on pjs.id = pjq.pwa_journey_step_id 
-
-left join pwa_journey_question_option pjqo 
-on pjq.id = pjqo.pwa_journey_question_id
-
-where pj.version = '1.0.0-naplo'
+go
