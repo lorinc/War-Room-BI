067d78baf3f6c4eb712ef230215d35fba84174ff
Lőrinc Nyitrai <lorinc@gmail.com>
2022-03-31 13:25:56 +0200

    Update util__every_journal_question.sql

--- util__every_journal_question.sql
+++ util__every_journal_question.sql
@@ -12,9 +12,11 @@ create or replace view util_every_journal_questions as
 			pjs.title as stitle,
 			pjq."text" as qtext,
 			pjq."textFieldLabel" as qtextlabel,
-			pjqo.value as ovalue
-			
-		from pwa_journey pj 
+			pjqo.value as ovalue,
+--			pja."text" as freetext_answer,
+			pjqooa.reference
+
+		from pwa_journey pj
 		
 		left join pwa_journey_step_group pjsg 
 		on pj.id = pjsg.pwa_journey_id 
@@ -28,11 +30,22 @@ create or replace view util_every_journal_questions as
 		left join pwa_journey_question_option pjqo 
 		on pjq.id = pjqo.pwa_journey_question_id
 		
-		where pj.version = '1.0.0-naplo'
+		left join pwa_journey_answer pja 
+		on pja.pwa_journey_question_id = pjq.id
+		and pja.pwa_journey_id = pj.id
+		
+		left join pwa_journey_question_option_of_answer pjqooa 
+		on pjqooa.pwa_journey_answer_id = pja.id
+		and pjqooa.pwa_journey_question_option_id = pjqo.id
+		
+		where pj.version in (
+			'v1.0.0-naplo', 
+			'v1.0.0-statusz'
+			)
 		
 		order by combined_order
 	)
-	
+
 	select * from results
 
 go
