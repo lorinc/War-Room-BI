bf9977586ed7212a077e270e63e565bcf958c98b
Lőrinc Nyitrai <lorinc@gmail.com>
2022-04-02 14:46:45 +0200

    Update vday_bi__votes.sql

--- vday_bi__votes.sql
+++ vday_bi__votes.sql
@@ -3,6 +3,7 @@ create or replace view vday_bi__votes as
 		select
 			oevk,
 			szk_address,
+			szk,
 			voting_location_id,
 			pwa_journey_step_title,
 			pwa_journey_question_option_value,
@@ -22,7 +23,7 @@ create or replace view vday_bi__votes as
 			and	jelolo_szerv is null)
 		and pwa_journey_question_option_value is not null
 		and voting_location_id is not null
-		group by 1,2,3,4,5,6
+		group by 1,2,3,4,5,6,7
 	)
 select * from results
 go
