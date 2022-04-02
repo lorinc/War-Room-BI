8a82e0051355af6844bd462ffa82a88d58891389
Lőrinc Nyitrai <lorinc@gmail.com>
2022-04-02 09:55:11 +0200

    Create vday_bi__votes.sql

---
+++ vday_bi__votes.sql
@@ -0,0 +1,28 @@
+create or replace view vday_bi__votes as
+	with results as (
+		select
+			oevk,
+			szk_address,
+			voting_location_id,
+			pwa_journey_step_title,
+			pwa_journey_question_option_value,
+			coalesce(
+				jelolo_szerv,
+				pwa_journey_question_option_value
+			) as jelolo_szerv,
+			max(pwa_journey_answer_text)::int as pwa_journey_answer_text
+		from vday_bi__journal vbj 
+		where
+			pwa_journey_step_title in (
+				'Nemzetiségi listás szavazás eredménye',
+				'Országos listás szavazás eredménye',
+				'Egyéni választókerületek eredménye')
+		and not (
+			pwa_journey_step_title = 'Egyéni választókerületek eredménye'
+			and	jelolo_szerv is null)
+		and pwa_journey_question_option_value is not null
+		and voting_location_id is not null
+		group by 1,2,3,4,5,6
+	)
+select * from results
+go
