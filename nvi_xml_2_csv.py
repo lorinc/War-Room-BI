b36799bbfb4476675824f9779fac4e7515ebae1d
Lőrinc Nyitrai <lorinc@gmail.com>
2022-04-03 18:23:05 +0200

    Update nvi_xml_2_csv.py

--- nvi_xml_2_csv.py
+++ nvi_xml_2_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 n = pd.read_xml('../input/nvi-xml/napkozi.xml')
-fn = napkozi.loc[:,['ido','maz','taz','sorsz','megj']].dropna()
+fn = n.loc[:,['ido','maz','taz','sorsz','megj']].dropna()
 fn.loc[:,['maz']] = fn.loc[:,['maz']].apply(lambda x: x.astype(int).astype(str).str.zfill(2))
 fn.loc[:,['taz','sorsz']] = fn.loc[:,['taz','sorsz']].apply(lambda x: x.astype(int).astype(str).str.zfill(3))
 fn['szk_id'] = 'ID2022'+fn.maz+fn.taz+fn.sorsz
